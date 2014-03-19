45fd9a60d2792e8511016ed2a5d97683b92d3659
James Koppen <jk@jameskoppen.com>
2014-03-19 14:03:53 -0500

    Jimmies bot successfully mutated into Yoshi bot

---
+++ yoshi.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'chatterbot/dsl'
+
+# remove this to send out tweets
+# debug_mode
+
+# remove this to update the db
+no_update
+# remove this to get less output when running
+verbose
+
+# here's a list of users to ignore
+#blacklist "abc", "def"
+
+# here's a list of things to exclude from searches
+# exclude "haha", "thank", "thanks", "stop", "lol"
+
+#search "'my jimmies'", opts={:count => 1} do |tweet|
+#  reply "#USER# " + pick_random_line(path+"responses.txt"), tweet
+#end
+
+replies do |tweet|
+  reply "#USER# " + "YOSHI", tweet
+end
+
+tweet "YOSHI"
