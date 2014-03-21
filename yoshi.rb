03805586c9d8a7be32c69ef0ef53c38f1605cf4a
James Koppen <jk@jameskoppen.com>
2014-03-21 08:03:42 -0500

    Not necessary to specify the Random module; there's only one rand function.

--- yoshi.rb
+++ yoshi.rb
@@ -8,18 +8,19 @@ require 'chatterbot/dsl'
 
 # remove this to update the db
 no_update
+
 # remove this to get less output when running
 #verbose
 
 # here's a list of users to ignore
 #blacklist "abc", "def"
 
+def yoshi(max)
+  rand(max).times.map{"YOSHI"}.join(" ")
+end
+
 # here's a list of things to exclude from searches
 # exclude "haha", "thank", "thanks", "stop", "lol"
-
-def yoshi(max)
-  Random.rand(max).times.map{"YOSHI"}.join(" ")
-end
 
 #search "yoshi", opts={:count => 1} do |tweet|
 #  reply "#USER# " + yoshi(12), tweet
